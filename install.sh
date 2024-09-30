bbd79116a2438b9d898a4c205673e1dd98b769ae
Abeer <abeergupta555@gmail.com>
2024-09-30 06:46:00 +0530

    updated script

--- install.sh
+++ install.sh
@@ -25,6 +25,12 @@ print_newlines() {
   printf "\n%.0s" $(seq 1 "$count")
 }
 
+# Display important note
+print_color 3 "IMPORTANT NOTE:"
+print_color 3 "This script only copies the Waybar configuration files to avoid breaking your system."
+print_color 3 "If you want to copy other configuration files, you will need to do so manually."
+print_newlines 2
+
 # Ask user if JakooLit's config is installed
 print_color_nonl 6 "Do you have JakooLit's config installed? (y/n): "
 read jakoolit_installed
@@ -43,15 +49,12 @@ if [[ "$jakoolit_installed" != [Yy]* ]]; then
 
   if [[ "$continue_anyway" != [Yy]* ]]; then
     print_color 1 "Installation aborted."
-    print_newlines 2
+    print_newlines 1
     exit 1
   fi
 fi
 
 # Continue with the rest of your script here
-print_color 2 "Proceeding with the installation..."
-
-# ... (rest of your script)
 print_color 2 "Proceeding with the installation..."
 
 # Define source and destination directories
@@ -95,3 +98,11 @@ copy_files "$WAYBAR_STYLE_SRC" "$WAYBAR_STYLE_DEST"
 
 print_color 2 "Installation completed successfully!"
 print_newlines 2
+
+# Add end message about changing Waybar layout and style
+print_color 3 "IMPORTANT:"
+print_color 3 "To complete the setup, please change the following:"
+print_color 4 "1. Waybar layout: Set to '[BOT] KDE' (ALT+SUPER+B)"
+print_color 4 "2. Waybar style: Set to '[Coloured] Translucent Alt' (CTRL+SUPER+B)"
+print_color 3
+print_newlines 1
